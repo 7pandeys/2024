aace1e17a6ecd0cce7955d94f590a54abdd50b46
Sandeep Pandey <71967467+7pandeys@users.noreply.github.com>
2024-08-19 08:01:50 +0530

    Feature/basic python (#2)
    * feat:Add Fraud data
    
    * Add prime Numbers
    
    * Add prime Numbers and install poetry
    
    * Add prime Numbers and install poetry
    
    * Add prime Numbers and install poetry
    
    * Add prime Numbers and install poetry
    
    * Add prime Numbers and install poetry
    
    * Add prime Numbers and install poetry
    
    * Add prime Numbers and install poetry
    
    * Add prime Numbers and install poetry
    
    * Add prime Numbers and install poetry
    
    ---------
    
    Co-authored-by: Sandeep <insights@Sandeeps-MacBook-Air.local>

---
+++ Basic/Basic_Of_Python/basic_of_python/__init__.py
@@ -0,0 +1,2 @@
+# This allows you to import prime_numbers directly from prime_module
+from .prime_numbers import prime_numbers
---
+++ Basic/Basic_Of_Python/tests/prime_test.py
@@ -0,0 +1,20 @@
+import pytest
+from basic_of_python.prime_numbers import prime_numbers
+
+def test_prime_number():
+    assert prime_numbers(11) == "11 is Prime Number"
+
+def test_non_prime_number():
+    assert prime_numbers(10) == "10 is Not Prime Number"
+
+def test_smallest_prime():
+    assert prime_numbers(2) == "2 is Prime Number"
+
+def test_one():
+    assert prime_numbers(1) == "1 is Prime Number"
+
+def test_zero():
+    assert prime_numbers(0) == "0 is Prime Number"
+
+def test_negative_number():
+    assert prime_numbers(-5) == "-5 is Prime Number"
